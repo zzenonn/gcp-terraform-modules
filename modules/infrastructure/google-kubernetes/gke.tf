b8a1b58fda18a5c097e031ef21a8bbd5c8950d92
zzenonn <miguel.saavedra@obf.ateneo.edu>
2021-08-05 15:37:08 +0800

    Add gke sample

---
+++ modules/infrastructure/google-kubernetes/gke.tf
@@ -0,0 +1,46 @@
+# GKE cluster
+resource "google_container_cluster" "primary" {
+  name     = "${data.google_project.project.name}-gke"
+  location = data.google_client_config.current.region
+  node_locations = data.google_compute_zones.available.names
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  network    = var.vpc_id
+  subnetwork = var.subnet_id
+
+  ip_allocation_policy {
+      cluster_ipv4_cidr_block = var.pod_ip_addr_range
+      services_ipv4_cidr_block = var.services_ip_addr_range
+  }
+}
+
+# Separately Managed Node Pool
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "${google_container_cluster.primary.name}-node-pool"
+  location = data.google_client_config.current.region
+
+  cluster    = google_container_cluster.primary.name
+  node_count = var.gke_num_nodes
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    labels = {
+      env = data.google_project.project.name
+    }
+
+    machine_type = var.node_type
+    tags         = ["gke-node", "${data.google_project.project.name}-gke"]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+}
---
+++ modules/infrastructure/google-kubernetes/variables.tf
@@ -0,0 +1,33 @@
+variable "gke_num_nodes" {
+  default     = 3
+  description = "Number of gke nodes"
+}
+
+variable "vpc_id" {
+  description = "VPC Network of the cluster"
+}
+
+variable "subnet_id" {
+  description = "Subnet to provision the cluster in"
+}
+
+variable "pod_ip_addr_range" {
+  default     = "192.168.0.0/16"
+  description = "IP address range used by pods"
+}
+
+variable "services_ip_addr_range" {
+  default     = "172.16.0.0/16"
+  description = "IP address range used by services"
+}
+
+variable "node_type" {
+  default     = "e2-medium"
+  description = "Node type used by noed group"
+}
+
+data "google_project" "project" {}
+
+data "google_compute_zones" "available" {}
+
+data "google_client_config" "current" {}
