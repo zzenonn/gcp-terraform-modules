2e38bb2e544dbba1a55c9cc62892dc930a671d2c
zzenonn <miguel.saavedra@obf.ateneo.edu>
2023-04-21 15:32:12 +0800

    Add min K8s version

--- modules/infrastructure/google-kubernetes/gke.tf
+++ modules/infrastructure/google-kubernetes/gke.tf
@@ -14,6 +14,7 @@ resource "google_container_cluster" "primary" {
 
   network    = var.vpc_id
   subnetwork = var.subnet_id
+  min_master_version = "1.26.3-gke.400"
 
   ip_allocation_policy {
       cluster_ipv4_cidr_block = var.pod_ip_addr_range
